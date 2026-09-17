e2301c5528f7e52e2b324d7f7cb9d7c2b7b82102
Otávio <manualswordotaconta@gmail.com>
2026-09-17 20:19:09 -0300

    Add files via upload

--- PROJETO/camera.h
+++ PROJETO/camera.h
@@ -10,6 +10,7 @@ class camera {
         double aspect_ratio = 1.0; // ratio largura sobre altura
         int image_width = 100; // img renderizada largura em pixels
         int samples_per_pixel = 10; // qtd de bglh aleatorio por pixel
+        int max_depth = 10; // profundidade max q pode ir
         
         void render(const hittable& world){
             initialize();
@@ -22,7 +23,7 @@ class camera {
                     color pixel_color(0, 0, 0);
                     for (int sample = 0; sample < samples_per_pixel; sample++){
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     write_color(std::cout, pixel_samples_scale * pixel_color);  // escrevendo as cores
                 }
@@ -84,7 +85,10 @@ class camera {
             return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
-        color ray_color(const ray& r, const hittable& world) const { 
+        color ray_color(const ray& r, int depth, const hittable& world) const { 
+            // se passar do limite de profundidade, n terá luz
+            if (depth <= 0)
+                return color(0, 0, 0);
             hit_record rec;
             
             // rec.t                  distância do raio até o impacto
@@ -92,9 +96,9 @@ class camera {
             // rec.normal             normal da superfície
             // rec.front_face         se o raio atingiu a frente da superfície
 
-            if (world.hit(r, interval(0, infinity), rec)) {
+            if (world.hit(r, interval(0.001, infinity), rec)) {
                 vec3 direction = random_on_hemisphere(rec.normal);
-                return 0.5 * ray_color(ray(rec.p, direction), world); // ex 0.5 * ((vec3(0,1,0) + vec3(1,1,1)) = (0.5, 1.0, 0.5)
+                return 0.5 * ray_color(ray(rec.p, direction), depth-1, world); // ex 0.5 * ((vec3(0,1,0) + vec3(1,1,1)) = (0.5, 1.0, 0.5)
             }
             
             vec3 unit_direction = unit_vector(r.direction());
